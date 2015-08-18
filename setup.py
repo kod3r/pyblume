2caf63823e5174b9e5d4246f2987d3ccbe8aa0fe
Michael Moore <michael@terbiumlabs.com>
2015-08-18 16:39:04 -0400

    Setup and LICENSE added

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
 from distutils.core import setup, Extension
-setup(name="pyblume", version="1.0",
-      ext_modules=[Extension("pyblume", ["pyblume.c", "murmur.c", "blume.c"])])
+setup(name="pyblume", 
+	version="1.0", 
+	url='http://terbiumlabs.com/pyblume', 
+	license='BSD', 
+	author="Terbium Labs", 
+	author_email="floss_dev@terbiumlabs.com", 
+	description='A fast, scalable Bloom Filter for Python', 
+	zip_safe=False,
+    include_package_data=True,
+    platforms='Linux',
+    ext_modules=[Extension("pyblume", ["pyblume.c", "murmur.c", "blume.c"])],
+	classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: C']
+    )
