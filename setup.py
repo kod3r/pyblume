e423142b45d94948c33ef6535fa082d8b9db54e6
Michael Moore <michael@terbiumlabs.com>
2015-08-18 17:16:34 -0400

    Updated version number after license fix

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup, Extension
 setup(name="pyblume",
-	version="1.0.7",
+	version="1.0.8",
 	url='http://terbiumlabs.com/pyblume',
 	license='BSD',
 	author="Terbium Labs",
